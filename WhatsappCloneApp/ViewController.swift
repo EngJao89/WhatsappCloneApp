2d0ef4be18b0db30cb7f5b8942bb967459049d3c
EngJao89 <eng_joao@outlook.com>
2025-01-20 22:50:27 -0300

    Initial Commit

---
+++ WhatsappCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsappCloneApp
+//
+//  Created by João Ricardo Martins Ribeiro on 20/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
