4c1e1decdb27c78adc1fad99cf490138b2d89008
EngJao89 <eng_joao@outlook.com>
2025-01-22 00:00:38 -0300

    create contact table of users

---
+++ WhatsappCloneApp/TableViewCell/ContatoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ContatoTableViewCell.swift
+//  WhatsappCloneApp
+//
+//  Created by João Ricardo Martins Ribeiro on 21/01/25.
+//
+
+import Foundation
+import UIKit
+
+class ContatoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fotoContato: UIImageView!
+    
+    @IBOutlet weak var textoNome: UILabel!
+    
+    @IBOutlet weak var textoEmail: UILabel!
+    
+    
+}
