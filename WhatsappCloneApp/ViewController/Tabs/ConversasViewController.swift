2db177724a18651d0a788a05641399914c8ee698
EngJao89 <eng_joao@outlook.com>
2025-01-24 00:00:57 -0300

    implements list of conversation

--- WhatsappCloneApp/ViewController/Tabs/ConversasViewController.swift
+++ WhatsappCloneApp/ViewController/Tabs/ConversasViewController.swift
@@ -13,7 +13,8 @@ import FirebaseFirestore
 class ConversasViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var tableViewConversas: UITableView!
-    
+    var listaConversas: [Dictionary<String, Any>] = []
+    var conversasListener: ListenerRegistration!
     
     var auth: Auth!
     var db: Firestore!
@@ -29,24 +30,72 @@ class ConversasViewController: UIViewController, UITableViewDelegate,UITableView
         // Do any additional setup after loading the view.
     }
     
+    func addListenerRecuperarConversas() {
+        
+        if let idUsuarioLogado = auth.currentUser?.uid {
+            
+            conversasListener = db.collection("conversas")
+            .document( idUsuarioLogado )
+            .collection("ultimas_conversas")
+                .addSnapshotListener { (querSnapshot, erro) in
+                    
+                    if erro == nil {
+                        
+                        self.listaConversas.removeAll()
+                        
+                        if let snapshot = querSnapshot {
+                            for document in snapshot.documents {
+                                let dados = document.data()
+                                print("dados \(dados)")
+                                self.listaConversas.append(dados)
+                            }
+                            self.tableViewConversas.reloadData()
+                        }
+                    }
+                    
+            }
+            
+            
+        }
+        
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.listaConversas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let celula = tableView.dequeueReusableCell(withIdentifier: "celulaConversa", for: indexPath) as! ConversaTableViewCell
         
-        celula.nomeConversa.text = "Jamilton Damasceno"
-        celula.ultimaConversa.text = "Me responde!!"
-        celula.fotoConversa.image = UIImage(named: "imagem-perfil")
+        let indice = indexPath.row
+        let dados = self.listaConversas[indice]
+        let nome = dados["nomeUsuario"] as? String
+        let ultimaMensagem = dados["ultimaMensagem"] as? String
         
+        celula.nomeConversa.text = nome
+        celula.ultimaConversa.text = ultimaMensagem
+        
+        if let urlFotoUsuario = dados["urlFotoUsuario"] as? String {
+            celula.fotoConversa.sd_setImage(with: URL(string: urlFotoUsuario), completed: nil)
+        }else{
+          celula.fotoConversa.image = UIImage(named: "imagem-perfil")
+        }
+
         return celula
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        addListenerRecuperarConversas()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        conversasListener.remove()
     }
     
 
